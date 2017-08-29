7f9b223ede4df32c4b96c91fc909e678f0965532
walidmash <waleedmash2@gmail.com>
2017-08-29 14:22:35 +0300

    remove useless lines

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
-const userListFromDatabase = require('./../model');
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('home', { users: userListFromDatabase });
+//  res.render('home', { users: userListFromDatabase });
 });
 
 module.exports = router;
