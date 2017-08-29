867788f6d4044a10cbe28f168f051b4a3641d421
walidmash <waleedmash2@gmail.com>
2017-08-29 14:11:13 +0300

    add main functions in app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const helper = require('handlebars-helpers')();
 const routes = require('./routes/index');
 const helpers = require('./views/helpers/index');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
 app.set('view engine', 'hbs');
+app.use(
+  express.static(path.join(__dirname, '..', 'public'), { maxAge: '30d' })
+);
 app.engine(
   'hbs',
   exphbs({
@@ -20,11 +24,8 @@ app.engine(
   })
 );
 
-// exphbs.registerHelper('upperCase', (name) => {
-//   return name.toUpperCase();
-// });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.use(routes);
 
 module.exports = app;
