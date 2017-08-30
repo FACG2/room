f1b0e8ce0fe11bd0415825581fe9e875359de0ec
walidmash <waleedmash2@gmail.com>
2017-08-30 10:02:09 +0300

    add update route and its partial

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,14 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   queries.allMessages((dbRes) => {
+    res.status(200);
     res.render('home.hbs', {allMessages: dbRes});
+  });
+});
+router.get('/update', (req, res) => {
+  queries.allMessages((dbRes) => {
+    res.status(200);
+    res.render('partials/allMessages.hbs', {allMessages: dbRes});
   });
 });
 router.post('/new', (req, res) => {
