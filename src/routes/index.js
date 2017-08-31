289566b4df126220452722c7c4c5281a84c4fa82
eslam3bed <iah-93@hotmail.com>
2017-08-31 09:28:26 +0300

    handling errors in /update router

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,28 +13,31 @@ router.get('/', (req, res) => {
     }
   });
 });
-router.get('/update', (req, res) => {
+router.get('/update', (req, res, next) => {
   queries.allMessages((err, dbRes) => {
-    // res.setHeader('Last-Modified', (new Date()).toUTCString());
-    res.render('partials/allMessages.hbs', {allMessages: dbRes, layout: false});
+    if (err) {
+      next(err);
+    } else {
+      res.render('partials/allMessages.hbs', {allMessages: dbRes, layout: false});
+    }
   });
 });
 
 router.post('/new', (req, res) => {
   res.status(302);
-  if(req.body.username.trim().length>1 || req.body.context.trim().length>1){
-  queries.storeMessage(req.body.username, req.body.context, (err, rows) => {
-    if (err) {
-      res.status(500);
-      res.write('Server Error');
-    } else {
-      console.log('aa', rows);
-      res.redirect('/');
-    }
-  });
-}else{
-  res.redirect('/');
-}
+  if (req.body.username.trim().length > 1 || req.body.context.trim().length > 1) {
+    queries.storeMessage(req.body.username, req.body.context, (err, rows) => {
+      if (err) {
+        res.status(500);
+        res.write('Server Error');
+      } else {
+        console.log('aa', rows);
+        res.redirect('/');
+      }
+    });
+  } else {
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
